a353c35b346aeabc4324ec60c259e32a95f75dde
Daniil Kozlov <daniil.a.kozlov@gmail.com>
2022-11-26 10:54:01 +0800

    Minor changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import re
 
 import requests
-from bs4 import BeautifulSoup, Tag
+from bs4 import BeautifulSoup
 from fake_headers import Headers
 from tqdm import tqdm
 
@@ -13,7 +13,7 @@ def parse_page(url: str):
     return BeautifulSoup(query, 'html.parser')
 
 
-def get_page_count(search_page: Tag):
+def get_page_count(search_page):
     attr = 'data-qa'
     page_wrappers = search_page.find_all(attrs={'data-qa': re.compile(r'pager-page-wrapper-.*')})
     try:
@@ -23,12 +23,12 @@ def get_page_count(search_page: Tag):
     return count
 
 
-def vacancy_has_all_keywords(post: Tag, *args):
+def vacancy_has_all_keywords(post, *args):
     description = post.find('div', attrs={'data-qa': 'vacancy-description'}).text.replace(u'\xa0', ' ')
     return all([key in description for key in args])
 
 
-def parse_vacancy(vacancy: Tag):
+def parse_vacancy(vacancy):
     company_name = vacancy.find('a', attrs={'data-qa': 'vacancy-company-name'})
     if company_name:
         company_name = company_name.text.replace(u'\xa0', ' ')
@@ -57,11 +57,11 @@ def main():
     count = get_page_count(first_page)
 
     result = []  # list of vacancies: href, salary, company_name, city
-    for page_number in tqdm(range(count + 1), desc='Search page', colour='blue'):
+    for page_number in range(count + 1):
         page = parse_page(f'{query_url}&page={page_number}')
         posts = page.find_all('div', class_='serp-item')
 
-        for post in tqdm(posts, desc=f'Vacancies on page {page_number}', colour='green'):
+        for post in tqdm(posts, desc=f'Vacancies on page {page_number}/{count+1}', colour='blue'):
             href = post.find_next('a', attrs={'data-qa': 'serp-item__title'}).get('href')
             vacancy = parse_page(href)
             if vacancy_has_all_keywords(vacancy, *keywords):
